3248ef0b006d09f9fc6673faa4c83cad62873236
Jatin Gumber <gumberjatin5@gmail.com>
2026-07-26 11:39:02 +0530

    Time: 9 ms (21.79%) | Memory: 47 MB (13.82%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,9 +1,7 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        while(s.contains(part)){
+        while(s.contains(part))
             s = s.replaceFirst(part,"");
-        }
         return s;
-        
     }
 }
