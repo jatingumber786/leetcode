3e5514bcfd7bc5115ff8151bfae6881517b4701d
Jatin Gumber <gumberjatin5@gmail.com>
2026-08-07 18:33:12 +0530

    Time: 1 ms (83.66%) | Memory: 42.8 MB (56.26%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] arr = s.split(" ");
+        String [] arr = s.split(" ");
         if(pattern.length()!=arr.length) return false;
         HashMap<Character,String> map = new HashMap<Character,String>();
         for(int i=0;i<pattern.length();i++){
             char ch = pattern.charAt(i);
-            boolean containskeyvariable = map.containsKey(ch);
-            if(map.containsValue(arr[i]) && !containskeyvariable) return false;
-            if(containskeyvariable && !map.get(ch).equals(arr[i])) return false;
+            boolean var = map.containsKey(ch);
+            if(map.containsValue(arr[i]) && !var ) return false;
+            else if(var && !map.get(ch).equals(arr[i])) return false;
             else map.put(ch,arr[i]);
         }
         return true;
