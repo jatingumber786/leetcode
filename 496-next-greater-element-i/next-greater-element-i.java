569adae29ac1f35503981a4a7d20e2f7a8f7abee
Jatin Gumber <gumberjatin5@gmail.com>
2026-08-20 10:31:57 +0530

    Time: 4 ms (74.17%) | Memory: 44.9 MB (81.33%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -6,9 +6,8 @@ class Solution {
             while(!st.isEmpty() && st.peek()<=nums2[i]){
                 st.pop();
             }
-            if(st.isEmpty())  map.put(nums2[i],-1);
+            if(st.isEmpty()) map.put(nums2[i],-1);
             else map.put(nums2[i],st.peek());
-
             st.push(nums2[i]);
         }
         int ans[] = new int[nums1.length];
@@ -16,5 +15,6 @@ class Solution {
             ans[i] = map.get(nums1[i]);
         }
         return ans;
+        
     }
 }
