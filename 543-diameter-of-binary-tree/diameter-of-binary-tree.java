8f2fd0c949ed4f2b3e8d8f048c6877a6853127d8
Jatin Gumber <gumberjatin5@gmail.com>
2026-09-10 11:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (27.69%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,15 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-        int daimeter=0;
-
+    int d =0;
     public int diameterOfBinaryTree(TreeNode root) {
-        height(root);
-        return daimeter;
+        dia(root);
+        return d;   
     }
-    private int height(TreeNode root){
+    private int dia(TreeNode root){
         if(root==null) return 0;
-        int lh = height(root.left);
-        int rh = height(root.right);
-        daimeter = Math.max(daimeter,lh+rh);
+        int lh = dia(root.left);
+        int rh = dia(root.right);
+        d = Math.max(d,lh+rh);
         return 1+Math.max(lh,rh);
     }
 }
