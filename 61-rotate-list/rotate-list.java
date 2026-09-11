ec3a855abc6ac06751eed8f00a2c5fa0d302bd76
Jatin Gumber <gumberjatin5@gmail.com>
2026-09-11 17:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (62.53%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,29 +1,23 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        int length =1;
-        if(head == null) 
-        return head;
-        //dummyis created to calculate length and to points towards last node
         ListNode dummy = head;
-        while(dummy.next!=null)
-        {
+        int length = 1;
+        if(head==null || head.next==null) return head;
+        while(dummy.next!=null){
+            length++;
             dummy = dummy.next;
-             length++;
         }
+         k = k%length; 
+        if(k==0) return head;
+        int steps = length-k;
         ListNode current = head;
-        k = k % length;
-        if(k==0)
-        return head;
-        //list node is created to find the node that points toward null which break the co
-        //connections for example 3
-        int steps = length - k;
         for(int i=1;i<steps;i++){
             current = current.next;
         }
         ListNode newHead = current.next;
-        current.next =null;
+        current.next = null;
         dummy.next = head;
-
         return newHead;
     }
+    
 }
