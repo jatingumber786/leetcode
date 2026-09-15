36dfb993ff3fef56d6de44457895f2d4dda32186
Jatin Gumber <gumberjatin5@gmail.com>
2026-09-15 17:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (5.15%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -1,37 +1,36 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         TreeNode node = root;
-        if(root==null) return null;
-        if(root.val==key) return help(root);
+        if(node == null) return null;
+        if(node.val==key) return help(root);
         while(node!=null){
             if(node.val>key){
-                if(node.left!=null && node.left.val == key){
-                    node.left =  help(node.left);
-                    break;
+                if(node.left!=null && node.left.val==key){
+                    node.left = help(node.left);
                 }else{
                     node = node.left;
                 }
             }else{
                 if(node.right!=null && node.right.val==key){
                     node.right = help(node.right);
-                    break;
                 }else{
                     node = node.right;
                 }
             }
         }
-        return root;
+        return root; 
     }
-    public TreeNode help(TreeNode root){
+    private TreeNode help(TreeNode root){
+        //TreeNode node = root;
         if(root.left==null) return root.right;
         else if(root.right==null) return root.left;
         TreeNode rightchild = root.right;
-        TreeNode lastchild = findlastchild(root.left);
+        TreeNode lastchild = findlastright(root.left);
         lastchild.right = rightchild;
         return root.left;
     }
-    public TreeNode findlastchild(TreeNode root){
+    private TreeNode findlastright(TreeNode root){
         if(root.right==null) return root;
-        return findlastchild(root.right);
+        return findlastright(root.right);
     }
 }
